1149c5b220d55677934f022718d128ea5c61a0b9
proprat <prathamrathore183@gmail.com>
2024-09-13 18:22:44 +0530

    cors 2

--- src/socket.jsx
+++ src/socket.jsx
@@ -7,8 +7,8 @@ const SocketContext = createContext();
 const getSocket = () => useContext(SocketContext);
 
 const SocketProvider = ({ children }) => {
-  const socket = useMemo(() => io(server, { withCredentials: true }), []);
-
+  const socket = useMemo(() => io(server, { withCredentials: true ,transports: ['websocket'],}), []);
+  
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
   );
